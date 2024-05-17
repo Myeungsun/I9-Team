8a874a9fd013d8f4a56134733391c5642e2c5670
ha-yang9 <imiyoung909@gmail.com>
2024-05-17 20:09:20 +0900

    Create GameManager.cs

---
+++ Assets/Skripts/GameManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    public GameObject meteor;
+
+    private void Awake()
+    {
+        Instance = this;
+        Time.timeScale = 1.0f;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("MakeRain", 0f, 1f);
+        MakeMeteor();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void MakeMeteor()
+    {
+        Instantiate(meteor);
+    }
+}
