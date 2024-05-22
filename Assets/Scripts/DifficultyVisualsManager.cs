1b2b4c84532bc4d76fef989c1ba818c0465270f6
somrang2 <167191895+somrang2@users.noreply.github.com>
2024-05-23 03:14:38 +0900

    fix
    수정

---
+++ Assets/Scripts/DifficultyVisualsManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class DifficultyVisualsManager : MonoBehaviour
+{
+    public GameObject easyBackground;
+    public GameObject normalBackground;
+    public GameObject hardBackground;
+
+    void Start()
+    {
+        SetDifficultyVisuals();
+    }
+
+    private void SetDifficultyVisuals()
+    {
+        FallingObjectManager.Difficulty savedDifficulty = (FallingObjectManager.Difficulty)PlayerPrefs.GetInt("GameDifficulty", (int)FallingObjectManager.Difficulty.Normal);
+
+        // 모든 배경 오브젝트를 비활성화
+        easyBackground.SetActive(false);
+        normalBackground.SetActive(false);
+        hardBackground.SetActive(false);
+
+        // 저장된 난이도에 따라 해당하는 배경 오브젝트를 활성화
+        switch (savedDifficulty)
+        {
+            case FallingObjectManager.Difficulty.Easy:
+                easyBackground.SetActive(true);
+                break;
+            case FallingObjectManager.Difficulty.Normal:
+                normalBackground.SetActive(true);
+                break;
+            case FallingObjectManager.Difficulty.Hard:
+                hardBackground.SetActive(true);
+                break;
+        }
+    }
+}
